7e996b874561af901a2ce3166bee565bb93c48aa
Krzysztof Kolodziejczyk <kkolodziejczyk@gog.com>
2023-07-29 21:36:26 +0200

    add temida records and pop3 ovh monitor

--- variables.tf
+++ variables.tf
@@ -18,6 +18,20 @@ variable "dns_records" {
             fieldtype = "A"
             ttl       = "300"
             target    = "130.61.139.21"
+        },
+        temida_czestochowa_pl = {
+            zone      = "temida-czestochowa.pl"
+            subdomain = ""
+            fieldtype = "A"
+            ttl       = "300"
+            target    = "130.61.139.21"
+        },
+        www_temida_czestochowa_pl = {
+            zone      = "temida-czestochowa.pl"
+            subdomain = "www"
+            fieldtype = "A"
+            ttl       = "300"
+            target    = "130.61.139.21"
         }
     }
 }
@@ -32,6 +46,12 @@ variable "custom_monitors" {
             type          = "port"
             sub_type      = "custom"
             port          = "22"
+        },
+        pop3_ssl0_ovh_net = {
+            url           = "ssl0.ovh.net"
+            name          = "POP3 ssl0.ovh.net"
+            type          = "port"
+            sub_type      = "pop3"
         }
     }
 }
